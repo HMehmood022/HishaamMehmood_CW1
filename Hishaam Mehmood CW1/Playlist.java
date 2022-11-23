bdc5d1a08bea59b215a30b48a9e110d3350327d8
HMehmood022 <114756188+HMehmood022@users.noreply.github.com>
2022-11-23 22:21:28 +0000

    CW1- Hishaam Mehmood

--- Hishaam Mehmood CW1/Playlist.java
+++ Hishaam Mehmood CW1/Playlist.java
@@ -5,13 +5,13 @@ public class Playlist {
     
     private static ArrayList<Song> songList = new ArrayList<>();/*The ArrayList class is used to create lists of elements. 
     An ArrayList is a resizable array which can change its size dynamically. We used the ArrayList because it allows us to store data, also known as "elements". */
-    private static ArrayList<Popularsong> PopularList = new ArrayList<>();
+
+    private static Scanner sc = new Scanner (System.in);
 
     public static void main(String[] args) 
     {
         int userSelected;/*An integer "Userselected" is made. The integer is used to store the option numbers. */
         add10Songs(); 
-        popularSongs();
 
 
         do 
@@ -30,13 +30,13 @@ public class Playlist {
                     removeSong();
                     break;
                 case 4:
-                    listPopularSongs();
+                    printPopularSongs();
                     break;
                 default:
                     break;
             }
         }
-        while(userSelected > 4);/*In order to ensure the user cannot enter a value higher than the amount of options given, 
+        while(userSelected < 5);/*In order to ensure the user cannot enter a value higher than the amount of options given, 
         I specified that anything more than 4 will have no return. 
         And if the user enters an option less than 1 they will recieve no return. */ 
     
@@ -45,54 +45,25 @@ public class Playlist {
 
     private static void removeSong() 
     {
-        Scanner sc = new Scanner(System.in); /*Here we use the scanner to receive input from the user, in this context the user is removing a song from the list. */
-        String removeSong;
+        /*make song list */
+         System.out.println("What song number would you like to remove?");
+         int removeSong = sc.nextInt();
+        
+         songList.remove(removeSong-1);
 
-         System.out.println("What song would you like to remove?");
-         removeSong = sc.nextLine();
-
-         System.out.println(" Removed the song " + removeSong + " from the songlist. ");
+         System.out.println("Removed song " + removeSong + " from the songlist. ");
 
     }
 
-    private static void popularSongs() 
+    private static void printPopularSongs() 
     {
+        System.out.println("Please enter the number of plays");
+        int plays = sc.nextInt();
 
-        Popularsong popularSongs = new Popularsong("10: Thriller", "Micheal Jackson", 7876643);/*The strings and integar from the popularsongs java file were imported.
-        As you can see below, the String Populartitle, String Popularartist and Integar popularplays are visible.
-        I created a new arraylist called "Popularsongs" which will list all the elements i.e songs in order based on their number of "plays". */
-        PopularList.add(popularSongs);
-
-        popularSongs = new Popularsong("9: Angels", "Tom Walker", 7487664);
-        PopularList.add(popularSongs);
-
-        popularSongs = new Popularsong("8: Rap God", "Eminem", 6209543);
-        PopularList.add(popularSongs);
-
-
-        popularSongs = new Popularsong("7: Bad Guy", "Billie Eilish", 4560942);
-        PopularList.add(popularSongs);
-
-
-        popularSongs = new Popularsong("6: Anti-Hero", "Taylor Swift", 3599493);
-        PopularList.add(popularSongs);
-
-
-        popularSongs = new Popularsong("5: Lift Me Up", "Rihana", 2528892);
-        PopularList.add(popularSongs);
-
-
-        popularSongs = new Popularsong("4: Blood On My Jeans", "Juice WRLD", 2524431);
-        PopularList.add(popularSongs);
-
-        popularSongs = new Popularsong("3: Cuff It", "Beyonce",2351592);
-        PopularList.add(popularSongs);
-
-        popularSongs = new Popularsong("2: On and On ft Daniel Levi", "Cartoon", 2325556);
-        PopularList.add(popularSongs);
-
-        popularSongs = new Popularsong("1: Hide And Seek", "Stormzy", 2324512);
-        PopularList.add(popularSongs);
+        for(Song song: songList)
+        {
+           if(song.getPlays() > plays) song.display();
+        }
     }   
 
 
@@ -129,68 +100,65 @@ public class Playlist {
     public static void addSong()
     {
         Scanner sc = new Scanner(System.in);/*Here I used the scanner class in  */
-        String songAdd;
+        String songTitle;
 
-        System.out.println("What song would you like to add? ");
-        songAdd = sc.nextLine();
+        System.out.println("What song title would you like to add? ");
+        songTitle = sc.nextLine();
 
-        System.out.println("Added Song " + songAdd + " to the list. ");
+        System.out.println("Please enter song artist: ");
+        String artist = sc.nextLine();
+        System.out.println("Please enter the number of plays: ");
+        int plays = sc.nextInt();
+
+        System.out.println("Your chosen song has been added to the list. Please enter the number 1 to view it in the songlist.");
+            Song song = new Song( " 11. " + songTitle, artist, plays );
+
+        songList.add(song);
+
 
     }
 
     public static void add10Songs()/*this method will list all songs. */
     {
-        Song song = new Song("Thriller", "Micheal Jackson", 7876643);/*the Song class is imported from the Song.java file. 
+        Song song = new Song(" 1. Thriller ", " Micheal Jackson ", 7876643);/*the Song class is imported from the Song.java file. 
         When adding songs the list, we can see the Strings and integar we created in the Song.Java file (String title, String Artist and Int plays.) An integar is used in order to display numbers.*/
 
         songList.add(song); //*ArrayList name*.add() is used to add the specified song to the ArrayList.
 
-        song = new Song("Rap God", "Eminem", 6209543);
+        song = new Song(" 2. Rap God ", " Eminem ", 620954);
 
         songList.add(song);
 
-        song = new Song("Angels", "Tom Walker", 7487664);
+        song = new Song(" 3. Angels ", " Tom Walker ", 748766);
 
         songList.add(song);
 
-        song = new Song("Bad Guy", "Billie Eilish", 4560942);
+        song = new Song(" 4. Bad Guy ", " Billie Eilish ", 456094);
 
         songList.add(song);
 
-        song = new Song("Anti-Hero", "Taylor Swift", 3599493);
+        song = new Song(" 5. Anti-Hero ", " Taylor Swift ", 359949);
 
         songList.add(song);
 
-        song = new Song("Lift Me Up", "Rihana", 2528892);
+        song = new Song(" 6. Lift Me Up ", " Rihana ", 25288);
 
         songList.add(song);
 
-        song = new Song("Cuff It", "Beyonce", 2351592);
+        song = new Song(" 7. Cuff It ", " Beyonce ", 235159);
 
         songList.add(song);
 
-        song = new Song("On and On ft Daniel Levi", "Cartoon", 2325556);
+        song = new Song(" 8. On and On ft Daniel Levi ", " Cartoon ", 232555);
 
         songList.add(song);
 
-        song = new Song("Blood On My Jeans", "Juice WRLD", 2524431);
+        song = new Song(" 9. Blood On My Jeans ", " Juice WRLD ", 252443);
 
         songList.add(song);
 
-        song = new Song("Hide And Seek", "Stormzy", 2324512);
+        song = new Song(" 10. Hide And Seek ", " Stormzy ", 232451);
 
         songList.add(song);
     }
-
-    public static void listPopularSongs()//This method will be used to display the popular songs.
-    {
-        System.out.println("Here is a popularity list of the songs, based on the amount of plays.");
-
-        for(Popularsong popularSongs: PopularList)
-        {
-            popularSongs.populardisplay();/*This will display the Popularsongs arraylist which we created at the top, all the songs are sorted via their number of plays. */
-        }
-    }
-
-        
 } 
--- Hishaam Mehmood CW1/Song.java
+++ Hishaam Mehmood CW1/Song.java
@@ -13,9 +13,13 @@ public class Song /* I created a song class for our main songList ArrayList */
 
     public void display()
     {
-        System.out.println(title + " by "+ artist + " " + plays); /*When adding the songs to the arraylist, the information is seperated by their values. 
+        System.out.println( title + " by " + artist + " " + plays ); /*When adding the songs to the arraylist, the information is seperated by their values. 
         i.e the song title will appear as title: *Song name* artist: *artist name* plays: *number of plays*/
 
     }
-    
+   
+    public int getPlays()
+    {
+        return plays;
+    }
 }
